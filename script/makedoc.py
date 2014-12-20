fe6c9413141a3b834c421ab87b80784fd3db1be3
Benedikt Seidl <Benedikt.Seidl@gmx.de>
2014-12-20 13:47:33 +0100

    include classes in doc.txt

--- script/makedoc.py
+++ script/makedoc.py
@@ -14,4 +14,13 @@ with open("doc.txt", "wb") as o:
         if hasattr(element, "__doc__"):
             doc = element.__doc__
             o.write(str(doc))
+        if hasattr(element, "__class__"):
+            for n in filter(lambda x: x[0] != "_", sorted(dir(element))):
+                o.write("\n")
+                o.write("- " * 20)
+                o.write("\n")
+                o.write(n)
+                e = getattr(element, n)
+                if hasattr(e, "__doc__"):
+                    o.write(str(e.__doc__))
 
